0144fe3466137131aa441deeefcd3ac89e749a7c
carllucasyu <carllucasyu@gmail.com>
2018-08-01 09:34:48 +0800

    BaseCreatedInfoModel 里面的字段改为更新忽略

--- src/main/java/org/linuxprobe/crud/model/BaseCreatedInfoModel.java
+++ src/main/java/org/linuxprobe/crud/model/BaseCreatedInfoModel.java
@@ -1,6 +1,7 @@
 package org.linuxprobe.crud.model;
 
 import java.util.Date;
+import org.linuxprobe.crud.persistence.annotation.Column;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,10 @@ import lombok.Setter;
 @Setter
 public class BaseCreatedInfoModel extends BaseModel {
 	/** 创建时间 */
+	@Column(updateIgnore = true)
 	private Date createTime;
 	/** 创建人id */
+	@Column(updateIgnore = true)
 	private String createrId;
 
 }
