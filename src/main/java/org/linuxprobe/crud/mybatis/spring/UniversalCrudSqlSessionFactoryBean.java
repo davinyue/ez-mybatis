8b3998a4b662d0c9a82badccf3e9952a03c1dd01
LarryYu <linuxprobe.org@gmail.com>
2019-01-16 00:42:06 +0800

    update

--- src/main/java/org/linuxprobe/crud/mybatis/spring/UniversalCrudSqlSessionFactoryBean.java
+++ src/main/java/org/linuxprobe/crud/mybatis/spring/UniversalCrudSqlSessionFactoryBean.java
@@ -23,7 +23,6 @@ import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.type.TypeHandler;
