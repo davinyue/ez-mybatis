55dd60cf4f8d6563cf6bcdbf54162b791e314597
LarryYu <linuxprobe.org@gmail.com>
2019-05-20 22:52:26 +0800

    sql转义更新

--- src/main/java/org/linuxprobe/crud/utils/SqlEscapeUtil.java
+++ src/main/java/org/linuxprobe/crud/utils/SqlEscapeUtil.java
@@ -2,12 +2,12 @@ package org.linuxprobe.crud.utils;
 
 /** sql关键字转义工具 */
 public class SqlEscapeUtil {
-	private static String[] fbsArr = { "\\", "'" };
+	private static String[] mysqlFbsArr = { "\\", "'" };
 
 	/** 转义 */
 	public static String mysqlEscape(String source) {
 		if (source != null) {
-			for (String key : fbsArr) {
+			for (String key : mysqlFbsArr) {
 				if (source.contains(key)) {
 					source = source.replace(key, "\\" + key);
 				}
