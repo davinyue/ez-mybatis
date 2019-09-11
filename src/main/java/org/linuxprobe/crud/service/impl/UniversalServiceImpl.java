61782ec1418178bd443d4893c474a90c45071b8c
larryyu <larryyu@yntengyun.com>
2019-09-11 17:20:46 +0800

    移除事务注解

--- src/main/java/org/linuxprobe/crud/service/impl/UniversalServiceImpl.java
+++ src/main/java/org/linuxprobe/crud/service/impl/UniversalServiceImpl.java
@@ -6,8 +6,6 @@ import org.linuxprobe.crud.core.query.Page;
 import org.linuxprobe.crud.mybatis.spring.UniversalCrudSqlSessionTemplate;
 import org.linuxprobe.crud.service.UniversalService;
 import org.linuxprobe.luava.reflection.ReflectionUtils;
-import org.springframework.aop.framework.AopContext;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.Collection;
@@ -33,84 +31,54 @@ public class UniversalServiceImpl<Model, IdType extends Serializable, Query exte
         return UniversalCrudContent.getEntityInfo(ReflectionUtils.getGenericSuperclass(this.getClass(), 0).getName()).getEntityType();
     }
 
-    /**
-     * 获取当前对象的代理对象,用代理对象调用代替this调用,可解决内部调用切面无效问题
-     */
-    public UniversalService<Model, IdType, Query> getProxy() {
-        if (this.proxy == null) {
-            this.proxy = (UniversalService<Model, IdType, Query>) AopContext.currentProxy();
-        }
-        return this.proxy;
-    }
-
-    /**
-     * 获取当前对象的代理对象,用代理对象调用代替this调用,可解决内部调用切面无效问题
-     */
-    public <T> T getProxy2() {
-        if (this.proxy == null) {
-            this.proxy = (UniversalService<Model, IdType, Query>) AopContext.currentProxy();
-        }
-        return (T) this.proxy;
-    }
-
     @Override
-    @Transactional
     public Model save(Model model) {
         return this.sqlSessionTemplate.insert(model);
     }
 
     @Override
-    @Transactional
     public List<Model> batchSave(List<Model> models) {
         return this.sqlSessionTemplate.batchInsert(models);
     }
 
     @Override
-    @Transactional
     public int removeByPrimaryKey(IdType id) {
         Class<?> modelClass = this.getModelCalss();
         return this.sqlSessionTemplate.deleteByPrimaryKey(id, modelClass);
     }
 
     @Override
-    @Transactional
     public long batchRemoveByPrimaryKey(List<IdType> ids) throws Exception {
         Class<?> modelClass = this.getModelCalss();
         return this.sqlSessionTemplate.batchDeleteByPrimaryKey((Collection<Serializable>) ids, modelClass);
     }
 
     @Override
-    @Transactional
     public int remove(Model record) {
         return this.sqlSessionTemplate.delete(record);
     }
 
     @Override
-    @Transactional
     public int batchRemove(List<Model> records) {
         return this.sqlSessionTemplate.batchDelete(records);
     }
 
     @Override
-    @Transactional
     public int removeByColumnName(String columnName, Serializable columnValue) {
         return this.sqlSessionTemplate.deleteByColumnName(columnName, columnValue, this.getModelCalss());
     }
 
     @Override
-    @Transactional
     public int removeByColumnNames(String[] columnNames, Serializable[] columnValues) {
         return this.sqlSessionTemplate.deleteByColumnNames(columnNames, columnValues, this.getModelCalss());
     }
 
     @Override
-    @Transactional
     public int removeyByFieldName(String fieldName, Serializable fieldValue) {
         return this.sqlSessionTemplate.deleteByFieldName(fieldName, fieldValue, this.getModelCalss());
     }
 
     @Override
-    @Transactional
     public int removeByFieldNames(String[] fieldNames, Serializable[] fieldValues) {
         return this.sqlSessionTemplate.deleteByFieldNames(fieldNames, fieldValues, this.getModelCalss());
     }
@@ -137,8 +105,8 @@ public class UniversalServiceImpl<Model, IdType extends Serializable, Query exte
         Page<Model> result = new Page<>();
         result.setCurrentPage(param.getLimit().getCurrentPage());
         result.setPageSize(param.getLimit().getPageSize());
-        result.setData(this.getProxy().getByQueryParam(param));
-        result.setTotal(this.getProxy().getCountByQueryParam(param));
+        result.setData(this.getByQueryParam(param));
+        result.setTotal(this.getCountByQueryParam(param));
         return result;
     }
 
@@ -183,13 +151,11 @@ public class UniversalServiceImpl<Model, IdType extends Serializable, Query exte
     }
 
     @Override
-    @Transactional
     public Model globalUpdate(Model model) {
         return this.sqlSessionTemplate.globalUpdate(model);
     }
 
     @Override
-    @Transactional
     public Model localUpdate(Model model) {
         return this.sqlSessionTemplate.localUpdate(model);
     }
