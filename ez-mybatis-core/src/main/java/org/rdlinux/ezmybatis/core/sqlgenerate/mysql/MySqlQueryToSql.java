21bf4f8a8a6b8d42659cffbba5ab82aec6c929c3
DavinYue <davinyue@outlook.com>
2021-12-01 21:15:13 +0800

    新增条件查询支持指定查询属性

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/sqlgenerate/mysql/MySqlQueryToSql.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/sqlgenerate/mysql/MySqlQueryToSql.java
@@ -2,14 +2,9 @@ package org.rdlinux.ezmybatis.core.sqlgenerate.mysql;
 
 import org.apache.ibatis.session.Configuration;
 import org.rdlinux.ezmybatis.core.EzQuery;
-import org.rdlinux.ezmybatis.core.content.EzEntityClassInfoFactory;
-import org.rdlinux.ezmybatis.core.content.entityinfo.EntityClassInfo;
 import org.rdlinux.ezmybatis.core.sqlgenerate.AbstractQueryToSql;
 import org.rdlinux.ezmybatis.core.sqlgenerate.MybatisParamHolder;
-import org.rdlinux.ezmybatis.core.sqlpart.Alias;
-import org.rdlinux.ezmybatis.core.sqlpart.EzFrom;
 import org.rdlinux.ezmybatis.core.sqlpart.EzLimit;
-import org.rdlinux.ezmybatis.core.sqlpart.EzTable;
 
 public class MySqlQueryToSql extends AbstractQueryToSql {
     private static volatile MySqlQueryToSql instance;
@@ -42,22 +37,22 @@ public class MySqlQueryToSql extends AbstractQueryToSql {
         }
         String keywordQM = this.getKeywordQM();
         sqlBuilder.append(" LIMIT ").append(limit.getSkip()).append(", ").append(limit.getSize());
-        EzFrom from = query.getFrom();
-        EzTable table = from.getTable();
-        String sql = sqlBuilder.toString();
-        int fromIndex = sql.indexOf(" FROM");
-        String fromSql = sql.substring(fromIndex);
-        EntityClassInfo entityClassInfo = EzEntityClassInfoFactory.forClass(configuration, table.getEtType());
-        String idColumn = entityClassInfo.getPrimaryKeyInfo().getColumnName();
-        String selectSql = "SELECT " + table.getAlias() + "." + keywordQM + idColumn + keywordQM;
-        String sonSql = selectSql + fromSql;
-        String sonAlias = Alias.getAlias();
-        String masterAlias = Alias.getAlias();
-        sqlBuilder = new StringBuilder("SELECT ").append(masterAlias).append(".* FROM ")
-                .append(entityClassInfo.getTableName()).append(" ").append(masterAlias)
-                .append(" INNER JOIN (").append(sonSql).append(") ").append(sonAlias).append(" ON ")
-                .append(sonAlias).append(".").append(keywordQM).append(idColumn).append(keywordQM).append(" = ")
-                .append(masterAlias).append(".").append(keywordQM).append(idColumn).append(keywordQM);
+//        EzFrom from = query.getFrom();
+//        EzTable table = from.getTable();
+//        String sql = sqlBuilder.toString();
+//        int fromIndex = sql.indexOf(" FROM");
+//        String fromSql = sql.substring(fromIndex);
+//        EntityClassInfo entityClassInfo = EzEntityClassInfoFactory.forClass(configuration, table.getEtType());
+//        String idColumn = entityClassInfo.getPrimaryKeyInfo().getColumnName();
+//        String selectSql = "SELECT " + table.getAlias() + "." + keywordQM + idColumn + keywordQM;
+//        String sonSql = selectSql + fromSql;
+//        String sonAlias = Alias.getAlias();
+//        String masterAlias = Alias.getAlias();
+//        sqlBuilder = new StringBuilder("SELECT ").append(masterAlias).append(".* FROM ")
+//                .append(entityClassInfo.getTableName()).append(" ").append(masterAlias)
+//                .append(" INNER JOIN (").append(sonSql).append(") ").append(sonAlias).append(" ON ")
+//                .append(sonAlias).append(".").append(keywordQM).append(idColumn).append(keywordQM).append(" = ")
+//                .append(masterAlias).append(".").append(keywordQM).append(idColumn).append(keywordQM);
         return sqlBuilder;
     }
 }
