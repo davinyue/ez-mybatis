21cf6ae6d9a0704d957b564d430061e8ae2cdc69
davinyue <davinyue@gmail.com>
2021-12-15 22:05:35 +0800

    更新通用查询

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/EzParam.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/EzParam.java
@@ -3,9 +3,11 @@ package org.rdlinux.ezmybatis.core;
 import lombok.Getter;
 import org.rdlinux.ezmybatis.core.sqlstruct.From;
 import org.rdlinux.ezmybatis.core.sqlstruct.Where;
+import org.rdlinux.ezmybatis.core.sqlstruct.table.EntityTable;
 
 @Getter
 public abstract class EzParam {
+    protected EntityTable table;
     protected From from;
     protected Where where;
 }
