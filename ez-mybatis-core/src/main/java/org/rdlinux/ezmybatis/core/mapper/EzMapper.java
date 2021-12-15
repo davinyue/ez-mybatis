649048ebde106e8a03ad9f617b8e52933bea102c
davinyue <davinyue@gmail.com>
2021-12-15 22:05:35 +0800

    更新通用查询

---
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/mapper/EzMapper.java
@@ -0,0 +1,20 @@
+package org.rdlinux.ezmybatis.core.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.rdlinux.ezmybatis.core.EzQuery;
+import org.rdlinux.ezmybatis.core.constant.EzMybatisConstant;
+import org.rdlinux.ezmybatis.core.mapper.provider.EzEntitySelectProvider;
+
+import java.util.List;
+
+/**
+ * 通用mapper
+ */
+public interface EzMapper {
+    @SelectProvider(type = EzEntitySelectProvider.class, method = "query")
+    <Rt> List<Rt> query(@Param(EzMybatisConstant.MAPPER_PARAM_QUERY) EzQuery query);
+
+    @SelectProvider(type = EzEntitySelectProvider.class, method = "query")
+    <Rt> Rt queryOne(@Param(EzMybatisConstant.MAPPER_PARAM_QUERY) EzQuery query);
+}
