a5a59e57f2d93d52bdadbe9d08103efc02635722
davinyue <davinyue@gmail.com>
2021-12-21 22:50:40 +0800

    优化条件构造器使用便利性

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/EzUpdate.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/EzUpdate.java
@@ -43,8 +43,22 @@ public class EzUpdate extends EzParam<Integer> {
             return this;
         }
 
+        public EzUpdateBuilder set(boolean sure, String field, Object value) {
+            if (sure) {
+                this.set(field, value);
+            }
+            return this;
+        }
+
         public EzUpdateBuilder setColumn(String column, Object value) {
             this.update.set.getItems().add(new UpdateColumnItem(this.update.table, column, value));
+            return this;
+        }
+
+        public EzUpdateBuilder setColumn(boolean sure, String column, Object value) {
+            if (sure) {
+                this.setColumn(column, value);
+            }
             return this;
         }
 
@@ -53,8 +67,22 @@ public class EzUpdate extends EzParam<Integer> {
             return this;
         }
 
+        public EzUpdateBuilder setSyntax(boolean sure, String field, String syntax) {
+            if (sure) {
+                this.setSyntax(field, syntax);
+            }
+            return this;
+        }
+
         public EzUpdateBuilder setColumnSyntax(String column, String syntax) {
             this.update.set.getItems().add(new SyntaxUpdateColumnItem(this.update.table, column, syntax));
+            return this;
+        }
+
+        public EzUpdateBuilder setColumnSyntax(boolean sure, String column, String syntax) {
+            if (sure) {
+                this.setColumnSyntax(column, syntax);
+            }
             return this;
         }
 
@@ -63,8 +91,22 @@ public class EzUpdate extends EzParam<Integer> {
             return this;
         }
 
+        public EzUpdateBuilder set(boolean sure, EntityTable table, String field, Object value) {
+            if (sure) {
+                this.set(table, field, value);
+            }
+            return this;
+        }
+
         public EzUpdateBuilder setColumn(Table table, String column, Object value) {
             this.update.set.getItems().add(new UpdateColumnItem(table, column, value));
+            return this;
+        }
+
+        public EzUpdateBuilder setColumn(boolean sure, Table table, String column, Object value) {
+            if (sure) {
+                this.setColumn(table, column, value);
+            }
             return this;
         }
 
@@ -73,8 +115,22 @@ public class EzUpdate extends EzParam<Integer> {
             return this;
         }
 
+        public EzUpdateBuilder setSyntax(boolean sure, EntityTable table, String field, String syntax) {
+            if (sure) {
+                this.setSyntax(table, field, syntax);
+            }
+            return this;
+        }
+
         public EzUpdateBuilder setColumnSyntax(EntityTable table, String field, String syntax) {
             this.update.set.getItems().add(new SyntaxUpdateColumnItem(table, field, syntax));
+            return this;
+        }
+
+        public EzUpdateBuilder setColumnSyntax(boolean sure, EntityTable table, String field, String syntax) {
+            if (sure) {
+                this.setColumnSyntax(table, field, syntax);
+            }
             return this;
         }
 
