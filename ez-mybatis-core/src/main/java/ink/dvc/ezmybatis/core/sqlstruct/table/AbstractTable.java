0e435840cdeb73b35f9065d04afbfd4c9673446b
Davin Yue <davinyue@gmail.com>
2021-12-22 17:46:57 +0800

    新增分区表查询支持

--- ez-mybatis-core/src/main/java/ink/dvc/ezmybatis/core/sqlstruct/table/AbstractTable.java
+++ ez-mybatis-core/src/main/java/ink/dvc/ezmybatis/core/sqlstruct/table/AbstractTable.java
@@ -3,10 +3,21 @@ package ink.dvc.ezmybatis.core.sqlstruct.table;
 import lombok.Getter;
 
 @Getter
-public class AbstractTable implements Table {
+public abstract class AbstractTable implements Table {
     protected String alias;
+    protected String partition;
 
     public AbstractTable(String alias) {
         this.alias = alias;
+    }
+
+    public AbstractTable(String alias, String partition) {
+        this.alias = alias;
+        this.partition = partition;
+    }
+
+    @Override
+    public String getPartition() {
+        return this.partition;
     }
 }
