8f67b6d6609cb55ab1855b6a8757142006ff3cd5
davinyue <davinyue@gmail.com>
2021-12-31 23:55:58 +0800

    优化代码

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/mapper/EzMapper.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/mapper/EzMapper.java
@@ -5,10 +5,10 @@ import org.rdlinux.ezmybatis.core.EzDelete;
 import org.rdlinux.ezmybatis.core.EzQuery;
 import org.rdlinux.ezmybatis.core.EzUpdate;
 import org.rdlinux.ezmybatis.core.constant.EzMybatisConstant;
-import org.rdlinux.ezmybatis.core.mapper.provider.EzEntityDeleteProvider;
-import org.rdlinux.ezmybatis.core.mapper.provider.EzEntityInsertProvider;
-import org.rdlinux.ezmybatis.core.mapper.provider.EzEntitySelectProvider;
-import org.rdlinux.ezmybatis.core.mapper.provider.EzEntityUpdateProvider;
+import org.rdlinux.ezmybatis.core.mapper.provider.EzDeleteProvider;
+import org.rdlinux.ezmybatis.core.mapper.provider.EzInsertProvider;
+import org.rdlinux.ezmybatis.core.mapper.provider.EzSelectProvider;
+import org.rdlinux.ezmybatis.core.mapper.provider.EzUpdateProvider;
 
 import java.util.List;
 import java.util.Map;
@@ -21,66 +21,71 @@ public interface EzMapper {
     /**
      * 根据sql插入记录
      */
-    @InsertProvider(type = EzEntityInsertProvider.class, method = "insertBySql")
+    @InsertProvider(type = EzInsertProvider.class, method = EzInsertProvider.INSERT_BY_SQL_METHOD)
     Integer insertBySql(@Param(EzMybatisConstant.MAPPER_PARAM_SQL) String sql,
                         @Param(EzMybatisConstant.MAPPER_PARAM_SQLPARAM) Map<String, Object> param);
 
     /**
      * 根据sql查询一条数据并返回map
      */
-    @SelectProvider(type = EzEntitySelectProvider.class, method = "selectBySql")
+    @SelectProvider(type = EzSelectProvider.class, method = EzSelectProvider.SELECT_BY_SQL_METHOD)
     Map<String, Object> selectOneMapBySql(@Param(EzMybatisConstant.MAPPER_PARAM_SQL) String sql,
                                           @Param(EzMybatisConstant.MAPPER_PARAM_SQLPARAM) Map<String, Object> param);
 
     /**
      * 根据sql查询多条数据, 并返回list map
      */
-    @SelectProvider(type = EzEntitySelectProvider.class, method = "selectBySql")
+    @SelectProvider(type = EzSelectProvider.class, method = EzSelectProvider.SELECT_BY_SQL_METHOD)
     List<Map<String, Object>> selectMapBySql(@Param(EzMybatisConstant.MAPPER_PARAM_SQL) String sql,
                                              @Param(EzMybatisConstant.MAPPER_PARAM_SQLPARAM) Map<String, Object> param);
 
-    @SelectProvider(type = EzEntitySelectProvider.class, method = "query")
+    @SelectProvider(type = EzSelectProvider.class, method = EzSelectProvider.QUERY_METHOD)
     <Rt> List<Rt> query(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) EzQuery<Rt> query);
 
-    @SelectProvider(type = EzEntitySelectProvider.class, method = "query")
+    @SelectProvider(type = EzSelectProvider.class, method = EzSelectProvider.QUERY_METHOD)
     <Rt> Rt queryOne(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) EzQuery<Rt> query);
 
     /**
      * 根据更新参数更新
      */
-    @UpdateProvider(type = EzEntityUpdateProvider.class, method = "updateByEzUpdate")
+    @UpdateProvider(type = EzUpdateProvider.class, method = EzUpdateProvider.UPDATE_BY_EZ_UPDATE_METHOD)
     int update(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) EzUpdate update);
 
     /**
      * 根据更新参数更新
      */
-    @UpdateProvider(type = EzEntityUpdateProvider.class, method = "batchUpdateByEzUpdate")
+    @UpdateProvider(type = EzUpdateProvider.class, method = EzUpdateProvider.BATCH_UPDATE_BY_EZ_UPDATE_METHOD)
     void batchUpdate(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) List<EzUpdate> updates);
 
     /**
      * 根据sql更新
      */
-    @UpdateProvider(type = EzEntityUpdateProvider.class, method = "updateBySql")
+    @UpdateProvider(type = EzUpdateProvider.class, method = EzUpdateProvider.UPDATE_BY_SQL_METHOD)
     Integer updateBySql(@Param(EzMybatisConstant.MAPPER_PARAM_SQL) String sql,
                         @Param(EzMybatisConstant.MAPPER_PARAM_SQLPARAM) Map<String, Object> param);
 
     /**
      * 批量删除
      */
-    @DeleteProvider(type = EzEntityDeleteProvider.class, method = "deleteByEzDelete")
+    @DeleteProvider(type = EzDeleteProvider.class, method = EzDeleteProvider.DELETE_BY_EZ_DELETE_METHOD)
     int delete(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) EzDelete delete);
 
     /**
      * 批量删除
      */
-    @DeleteProvider(type = EzEntityDeleteProvider.class, method = "batchDeleteByEzDelete")
+    @DeleteProvider(type = EzDeleteProvider.class, method = EzDeleteProvider.BATCH_DELETE_BY_EZ_DELETE_METHOD)
     void batchDelete(@Param(EzMybatisConstant.MAPPER_PARAM_EZPARAM) List<EzDelete> deletes);
 
     /**
      * 根据sql删除
      */
-    @DeleteProvider(type = EzEntityDeleteProvider.class, method = "deleteBySql")
+    @DeleteProvider(type = EzDeleteProvider.class, method = EzDeleteProvider.DELETE_BY_SQL_METHOD)
     Integer deleteBySql(@Param(EzMybatisConstant.MAPPER_PARAM_SQL) String sql,
                         @Param(EzMybatisConstant.MAPPER_PARAM_SQLPARAM) Map<String, Object> param);
 
+    /**
+     * 插入, 注意, 该接口仅能插入单条实体数据, 不能传入map或collection或array
+     */
+    //@InsertProvider(type = EzEntityInsertProvider.class, method = "insert")
+    //int insert(Object entity);
 }
