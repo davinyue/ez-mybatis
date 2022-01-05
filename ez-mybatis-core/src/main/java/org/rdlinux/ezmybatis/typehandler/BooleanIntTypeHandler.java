d902f00c18d3689f5e7c11a3a38528d25825a1a8
davinyue <davinyue@gmail.com>
2022-01-05 14:09:47 +0800

    修复了一些bug

---
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/typehandler/BooleanIntTypeHandler.java
@@ -0,0 +1,48 @@
+package org.rdlinux.ezmybatis.typehandler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.*;
+
+public class BooleanIntTypeHandler extends BaseTypeHandler<Boolean> {
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Boolean parameter, JdbcType jdbcType)
+            throws SQLException {
+        if (parameter == null) {
+            ps.setNull(i, Types.NULL);
+        } else {
+            ps.setInt(i, parameter ? 1 : 0);
+        }
+    }
+
+    @Override
+    public Boolean getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        int result = rs.getInt(columnName);
+        if (rs.wasNull()) {
+            return null;
+        } else {
+            return result != 0;
+        }
+    }
+
+    @Override
+    public Boolean getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        int result = rs.getInt(columnIndex);
+        if (rs.wasNull()) {
+            return null;
+        } else {
+            return result != 0;
+        }
+    }
+
+    @Override
+    public Boolean getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        int result = cs.getInt(columnIndex);
+        if (cs.wasNull()) {
+            return null;
+        } else {
+            return result != 0;
+        }
+    }
+}
