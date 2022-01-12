5e2039ae3b30615f9deae28315df4a6cca4e9da6
davinyue <davinyue@gmail.com>
2022-01-12 16:42:58 +0800

    解决ezmapper多线程查询结果类型冲突bug

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/interceptor/executor/ResultMapInitLogic.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/interceptor/executor/ResultMapInitLogic.java
@@ -12,14 +12,33 @@ import org.rdlinux.ezmybatis.core.mapper.EzMapper;
 import org.rdlinux.ezmybatis.utils.ReflectionUtils;
 
 import java.lang.reflect.Field;
-import java.util.Collections;
 import java.util.Map;
 
 public class ResultMapInitLogic implements InterceptorLogic {
-    private static final Field resultMapsField = ReflectionUtils.getField(MappedStatement.class,
-            "resultMaps");
+    /**
+     * 返回结果
+     */
+    public static final ThreadLocal<Class<?>> RET_TYPE_TL = new ThreadLocal<>();
     private static final Field hasNestedResultMapsFiled = ReflectionUtils.getField(ResultMap.class,
             "hasNestedResultMaps");
+
+    /**
+     * 获取结果类型
+     */
+    public static Class<?> getRetType(ResultMap resultMap) {
+        Class<?> retType = RET_TYPE_TL.get();
+        if (retType == null) {
+            retType = resultMap.getType();
+        }
+        return retType;
+    }
+
+    /**
+     * 清空结果类型
+     */
+    public static void cleanRetType() {
+        RET_TYPE_TL.remove();
+    }
 
     @Override
     @SuppressWarnings(value = {"rawtype", "unchecked"})
@@ -35,25 +54,22 @@ public class ResultMapInitLogic implements InterceptorLogic {
             if (resultMap.getResultMappings() != null && !resultMap.getResultMappings().isEmpty()) {
                 continue;
             }
+            Map<String, Object> param = (Map<String, Object>) invocation.getArgs()[1];
             //泛型接口, 需要动态的设置返回结果类型，这两个接口的返回类型由query参数传入
             if (resultMap.getId().startsWith(EzMapper.class.getName() + "." + EzMapper.QUERY_METHOD + "-") ||
                     resultMap.getId().startsWith(EzMapper.class.getName() + "." + EzMapper.QUERY_ONE_METHOD + "-")) {
-                Map<String, Object> param = (Map<String, Object>) invocation.getArgs()[1];
                 EzParam<?> ezParam = (EzParam<?>) param.get(EzMybatisConstant.MAPPER_PARAM_EZPARAM);
-                ResultMap newRm = new ResultMap.Builder(ms.getConfiguration(), resultMap.getId(),
-                        ezParam.getRetType(), resultMap.getResultMappings()).build();
-                ReflectionUtils.setFieldValue(ms, resultMapsField, Collections.singletonList(newRm), false);
+                RET_TYPE_TL.set(ezParam.getRetType());
+                ReflectionUtils.setFieldValue(resultMap, hasNestedResultMapsFiled, false);
             }
             //泛型接口, 需要动态的设置返回结果类型，这两个接口的返回类型是由参数参入的
             else if (resultMap.getId().startsWith(EzMapper.class.getName() + "." + EzMapper.SELECT_BY_ID_METHOD
                     + "-") ||
                     resultMap.getId().startsWith(EzMapper.class.getName() + "." + EzMapper.SELECT_BY_IDS_METHOD
                             + "-")) {
-                Map<String, Object> param = (Map<String, Object>) invocation.getArgs()[1];
                 Class<?> entityClass = (Class<?>) param.get(EzMybatisConstant.MAPPER_PARAM_ENTITY_CLASS);
-                ResultMap newRm = new ResultMap.Builder(ms.getConfiguration(), resultMap.getId(),
-                        entityClass, resultMap.getResultMappings()).build();
-                ReflectionUtils.setFieldValue(ms, resultMapsField, Collections.singletonList(newRm), false);
+                RET_TYPE_TL.set(entityClass);
+                ReflectionUtils.setFieldValue(resultMap, hasNestedResultMapsFiled, false);
             }
             //查询count, 需要把hasNestedResultMaps设置为false, 才能解析结果
             else if (resultMap.getId().startsWith(EzMapper.class.getName() + "." + EzMapper.QUERY_COUNT_METHOD
