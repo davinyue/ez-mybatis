8ca9e42f0a85dc93c9b288e9f7b55511587dc500
davinyue <davinyue@gmail.com>
2022-01-13 10:54:04 +0800

    修复ezMapper spring boot mapper自动注册失败bug

--- ez-mybatis-spring/src/main/java/org/rdlinux/ezmybatis/spring/SpringEzMybatisInit.java
+++ ez-mybatis-spring/src/main/java/org/rdlinux/ezmybatis/spring/SpringEzMybatisInit.java
@@ -11,32 +11,22 @@ import org.springframework.context.ApplicationContext;
 import java.util.Comparator;
 import java.util.Map;
 
-public class EzMybatisConfigurationCustom {
-    private Configuration configuration;
-    private ApplicationContext applicationContext;
-
-
-    public EzMybatisConfigurationCustom(Configuration configuration, ApplicationContext applicationContext) {
+public class SpringEzMybatisInit {
+    public static void init(Configuration configuration, ApplicationContext applicationContext) {
         Assert.notNull(configuration, "configuration can not be null");
         Assert.notNull(applicationContext, "applicationContext can not be null");
-        this.configuration = configuration;
-        this.applicationContext = applicationContext;
-        this.init();
-    }
-
-    private void init() {
-        EzMybatisContent.init(this.configuration);
-        Map<String, EzMybatisInsertListener> insertListenerMap = this.applicationContext.getBeansOfType(
+        EzMybatisContent.init(configuration);
+        Map<String, EzMybatisInsertListener> insertListenerMap = applicationContext.getBeansOfType(
                 EzMybatisInsertListener.class);
         insertListenerMap.values().stream().sorted(Comparator.comparingInt(EzMybatisInsertListener::order))
-                .forEach(e -> EzMybatisContent.addInsertListener(this.configuration, e));
-        Map<String, EzMybatisUpdateListener> updateListenerMap = this.applicationContext.getBeansOfType(
+                .forEach(e -> EzMybatisContent.addInsertListener(configuration, e));
+        Map<String, EzMybatisUpdateListener> updateListenerMap = applicationContext.getBeansOfType(
                 EzMybatisUpdateListener.class);
         updateListenerMap.values().stream().sorted(Comparator.comparingInt(EzMybatisUpdateListener::order))
-                .forEach(e -> EzMybatisContent.addUpdateListener(this.configuration, e));
-        Map<String, EzMybatisDeleteListener> deleteListenerMap = this.applicationContext.getBeansOfType(
+                .forEach(e -> EzMybatisContent.addUpdateListener(configuration, e));
+        Map<String, EzMybatisDeleteListener> deleteListenerMap = applicationContext.getBeansOfType(
                 EzMybatisDeleteListener.class);
         deleteListenerMap.values().stream().sorted(Comparator.comparingInt(EzMybatisDeleteListener::order))
-                .forEach(e -> EzMybatisContent.addDeleteListener(this.configuration, e));
+                .forEach(e -> EzMybatisContent.addDeleteListener(configuration, e));
     }
 }
