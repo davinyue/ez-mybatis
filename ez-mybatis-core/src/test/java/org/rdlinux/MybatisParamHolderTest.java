bfc291b83ff442e580a699c2359794c9c30a454a
davinyue <davinyue@gmail.com>
2022-06-06 16:14:05 +0800

    优化构建sql时,mybatis参数的存放，在一定程度上杜绝空间浪费

---
+++ ez-mybatis-core/src/test/java/org/rdlinux/MybatisParamHolderTest.java
@@ -0,0 +1,29 @@
+package org.rdlinux;
+
+import org.rdlinux.ezmybatis.core.sqlgenerate.MybatisParamHolder;
+
+import java.util.HashMap;
+
+public class MybatisParamHolderTest {
+    public static void main(String[] args) {
+        MybatisParamHolder paramHolder = new MybatisParamHolder(new HashMap<>());
+        String name = paramHolder.getParamName("a", false);
+        System.out.println(name);
+        name = paramHolder.getParamName("a", false);
+        System.out.println(name);
+        name = paramHolder.getParamName("a", false);
+        System.out.println(name);
+        name = paramHolder.getParamName("a", false);
+        System.out.println(name);
+        int oldCount = 10;
+        //新容量 = 旧容量>>1 + 旧容量
+        for (int i = 0; ; i++) {
+            int kr = oldCount >> 1;
+            oldCount = oldCount + kr;
+            System.out.println("扩容次数" + (i + 1) + ", 扩容后容量" + oldCount);
+            if (oldCount >= 65536) {
+                break;
+            }
+        }
+    }
+}
