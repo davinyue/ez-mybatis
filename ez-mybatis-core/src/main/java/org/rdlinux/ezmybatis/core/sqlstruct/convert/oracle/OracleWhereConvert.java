641ab264fdf165e4475b39feefa397c3106c54f9
davin <davinyue@gmail.com>
2022-08-02 14:25:36 +0800

    feat: 更新sql构造器

---
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/sqlstruct/convert/oracle/OracleWhereConvert.java
@@ -0,0 +1,8 @@
+package org.rdlinux.ezmybatis.core.sqlstruct.convert.oracle;
+
+import org.rdlinux.ezmybatis.core.sqlstruct.Where;
+import org.rdlinux.ezmybatis.core.sqlstruct.convert.Convert;
+import org.rdlinux.ezmybatis.core.sqlstruct.convert.mysql.MySqlWhereConvert;
+
+public class OracleWhereConvert extends MySqlWhereConvert implements Convert<Where> {
+}
