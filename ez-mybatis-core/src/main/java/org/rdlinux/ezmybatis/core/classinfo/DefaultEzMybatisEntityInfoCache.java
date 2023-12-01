1d1fdc957e55daef4375d46a72748957cc404b60
davin <davinyue@gmail.com>
2023-12-01 14:17:24 +0800

    feat: 支持idea运行时, 修改java bean属性时, 实现热更新, 不需要重启项目

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/classinfo/DefaultEzMybatisEntityInfoCache.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/classinfo/DefaultEzMybatisEntityInfoCache.java
@@ -1,9 +1,18 @@
 package org.rdlinux.ezmybatis.core.classinfo;
 
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.Configuration;
 import org.rdlinux.ezmybatis.core.classinfo.entityinfo.EntityClassInfo;
 import org.rdlinux.ezmybatis.utils.Assert;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -12,8 +21,73 @@ public class DefaultEzMybatisEntityInfoCache implements EzMybatisEntityInfoCache
     /**
      * 实体信息映射
      */
-    private static final ConcurrentMap<Configuration, ConcurrentMap<String, EntityClassInfo>> ENTITY_INFO_MAP =
+    protected static final ConcurrentMap<Configuration, ConcurrentMap<String, EntityClassInfo>> ENTITY_INFO_MAP =
             new ConcurrentHashMap<>();
+    private static final Log log = LogFactory.getLog(DefaultEzMybatisEntityInfoCache.class);
+
+    public DefaultEzMybatisEntityInfoCache() {
+        this.initClassFileChangeWatch();
+    }
+
+    /**
+     * 初始化类文件变更监控
+     */
+    protected void initClassFileChangeWatch() {
+        URL classLocation = DefaultEzMybatisEntityInfoCache.class.getResource("/");
+        if (classLocation.toString().startsWith("jar:file:")) {
+            return;
+        }
+        Thread cleanThread = new Thread(() -> {
+            try {
+                //需要监视的文件目录（注意：只能监听目录）
+                Path start = Paths.get(classLocation.toURI());
+                //创建监视服务类
+                WatchService ws = FileSystems.getDefault().newWatchService();
+                // 删除和修改事件
+                WatchEvent.Kind<?>[] kinds = {
+                        StandardWatchEventKinds.ENTRY_DELETE,
+                        StandardWatchEventKinds.ENTRY_MODIFY};
+                Map<WatchKey, Path> keys = new HashMap<>(16);
+                // 注册子目录到监视服务。
+                Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir,
+                                                             BasicFileAttributes attrs) throws IOException {
+                        keys.put(dir.register(ws, kinds), dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                while (true) {
+                    WatchKey watchKey = ws.take();
+                    Path path = keys.get(watchKey);
+                    String pathStr = path.toString();
+                    List<WatchEvent<?>> watchEvents = watchKey.pollEvents();
+                    String separator = String.valueOf(File.separatorChar);
+                    String classSubPath = "classes";
+                    String splitStr = separator + "target" + separator + classSubPath + separator;
+                    for (WatchEvent<?> event : watchEvents) {
+                        if (pathStr.contains(splitStr) && event.context().toString().endsWith(".class")) {
+                            String packageStr = pathStr.substring(pathStr.indexOf(splitStr) + splitStr.length())
+                                    .replace(separator, ".");
+                            String className = event.context().toString().split("\\.")[0];
+                            String key = packageStr + "." + className;
+                            for (ConcurrentMap<String, EntityClassInfo> cache : ENTITY_INFO_MAP.values()) {
+                                EntityClassInfo classInfo = cache.remove(key);
+                                if (classInfo != null && log.isDebugEnabled()) {
+                                    log.debug(String.format("Cleaning the class information of %s", key));
+                                }
+                            }
+                        }
+                    }
+                    watchKey.reset();
+                }
+            } catch (Exception ignored) {
+            }
+        });
+        cleanThread.setDaemon(true);
+        cleanThread.setName("ez-mybatis-entity-info-clean");
+        cleanThread.start();
+    }
 
     @Override
     public EntityClassInfo get(Configuration configuration, Class<?> ntClass) {
