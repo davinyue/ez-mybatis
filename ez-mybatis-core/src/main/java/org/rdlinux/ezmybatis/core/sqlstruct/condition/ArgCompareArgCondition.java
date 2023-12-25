3198efebb17e70147a63dae57d5562332813fd15
davin <davinyue@gmail.com>
2023-12-25 16:39:50 +0800

    feat: 优化左右值比较, 减少异常抛出

--- ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/sqlstruct/condition/ArgCompareArgCondition.java
+++ ez-mybatis-core/src/main/java/org/rdlinux/ezmybatis/core/sqlstruct/condition/ArgCompareArgCondition.java
@@ -1,6 +1,7 @@
 package org.rdlinux.ezmybatis.core.sqlstruct.condition;
 
 import lombok.Getter;
+import org.rdlinux.ezmybatis.core.sqlstruct.ObjArg;
 import org.rdlinux.ezmybatis.core.sqlstruct.Operand;
 import org.rdlinux.ezmybatis.core.sqlstruct.SqlStruct;
 import org.rdlinux.ezmybatis.utils.Assert;
@@ -25,11 +26,21 @@ public class ArgCompareArgCondition implements Condition, SqlStruct {
     private List<Operand> rightValues;
 
 
-    public ArgCompareArgCondition(LogicalOperator logicalOperator, Operand leftValue, Operator operator, Operand rightValue) {
+    public ArgCompareArgCondition(LogicalOperator logicalOperator, Operand leftValue, Operator operator,
+                                  Operand rightValue) {
         Assert.notNull(logicalOperator, "logicalOperator can not be null");
         Assert.notNull(leftValue, "leftValue can not be null");
         Assert.notNull(operator, "operator can not be null");
-        Assert.notNull(rightValue, "rightValue can not be null");
+        if (rightValue == null) {
+            //如果不是is null 查询或者in，右值不能为空
+            if (operator == Operator.isNull || operator == Operator.isNotNull ||
+                    operator == Operator.in || operator == Operator.notIn) {
+                rightValue = ObjArg.of(null);
+            } else {
+                throw new IllegalArgumentException("rightValue can not be null");
+            }
+        }
+        //不支持between查询
         if (operator == Operator.between || operator == Operator.notBetween) {
             throw new IllegalArgumentException("Unsupported relational operator");
         }
@@ -59,10 +70,14 @@ public class ArgCompareArgCondition implements Condition, SqlStruct {
         Assert.notNull(logicalOperator, "logicalOperator can not be null");
         Assert.notNull(leftValue, "leftValue can not be null");
         Assert.notNull(operator, "operator can not be null");
-        Assert.notNull(minValue, "minValue can not be null");
-        Assert.notNull(maxValue, "maxValue can not be null");
         if (operator != Operator.between && operator != Operator.notBetween) {
             throw new IllegalArgumentException("Unsupported relational operator");
+        }
+        if (minValue == null) {
+            minValue = ObjArg.of(null);
+        }
+        if (maxValue == null) {
+            maxValue = ObjArg.of(null);
         }
         this.logicalOperator = logicalOperator;
         this.leftValue = leftValue;
@@ -76,9 +91,11 @@ public class ArgCompareArgCondition implements Condition, SqlStruct {
         Assert.notNull(logicalOperator, "logicalOperator can not be null");
         Assert.notNull(leftValue, "leftValue can not be null");
         Assert.notNull(operator, "operator can not be null");
-        Assert.notEmpty(rightValues, "rightValues can not be empty");
         if (operator != Operator.in && operator != Operator.notIn) {
             throw new IllegalArgumentException("Unsupported relational operator");
+        }
+        if (rightValues == null || rightValues.isEmpty()) {
+            rightValues = Collections.singletonList(ObjArg.of(null));
         }
         this.logicalOperator = logicalOperator;
         this.leftValue = leftValue;
