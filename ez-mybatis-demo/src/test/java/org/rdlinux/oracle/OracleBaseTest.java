d2d1f0bd026c4698522d2a3c46cceed5387d2439
Davin Yue <davinyue@gmail.com>
2025-12-25 11:36:34 +0800

    feat: 更新测试用例

---
+++ ez-mybatis-demo/src/test/java/org/rdlinux/oracle/OracleBaseTest.java
@@ -0,0 +1,121 @@
+package org.rdlinux.oracle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.rdlinux.ezmybatis.EzMybatisConfig;
+import org.rdlinux.ezmybatis.core.EzDelete;
+import org.rdlinux.ezmybatis.core.EzMybatisContent;
+import org.rdlinux.ezmybatis.core.EzUpdate;
+import org.rdlinux.ezmybatis.core.interceptor.listener.EzMybatisDeleteListener;
+import org.rdlinux.ezmybatis.core.interceptor.listener.EzMybatisInsertListener;
+import org.rdlinux.ezmybatis.core.interceptor.listener.EzMybatisUpdateListener;
+import org.rdlinux.ezmybatis.demo.entity.BaseEntity;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.Date;
+
+@Slf4j
+public class OracleBaseTest {
+    protected static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        String resource = "mybatis-config-oracle.xml";
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XMLConfigBuilder parser = new XMLConfigBuilder(reader, null, null);
+        Configuration configuration = parser.parse();
+        EzMybatisConfig ezMybatisConfig = new EzMybatisConfig(configuration);
+        ezMybatisConfig.setEscapeKeyword(false);
+        EzMybatisContent.init(ezMybatisConfig);
+        EzMybatisContent.addInsertListener(ezMybatisConfig, new EzMybatisInsertListener() {
+            @Override
+            public void onInsert(Object entity) {
+                if (entity instanceof BaseEntity) {
+                    OracleBaseTest.log.info("插入事件");
+                    ((BaseEntity) entity).setUpdateTime(new Date());
+                    ((BaseEntity) entity).setCreateTime(new Date());
+                }
+            }
+
+            @Override
+            public void onBatchInsert(Collection<?> models) {
+                models.forEach(this::onInsert);
+            }
+        });
+        EzMybatisContent.addDeleteListener(ezMybatisConfig, new EzMybatisDeleteListener() {
+
+            @Override
+            public void onDelete(Object entity) {
+                OracleBaseTest.log.info("删除事件");
+            }
+
+            @Override
+            public void onBatchDelete(Collection<Object> models) {
+                models.forEach(this::onDelete);
+            }
+
+            @Override
+            public void onDeleteById(Object id, Class<?> ntClass) {
+                OracleBaseTest.log.info("删除事件");
+            }
+
+            @Override
+            public void onBatchDeleteById(Collection<Object> ids, Class<?> ntClass) {
+                for (Object id : ids) {
+                    this.onDeleteById(id, ntClass);
+                }
+            }
+
+            @Override
+            public void onEzDelete(EzDelete ezDelete) {
+                OracleBaseTest.log.info("ez_delete删除:{}", ezDelete.getTable().getTableName(configuration));
+            }
+        });
+        EzMybatisContent.addUpdateListener(ezMybatisConfig, new EzMybatisUpdateListener() {
+            @Override
+            public void onUpdate(Object entity) {
+                OracleBaseTest.log.info("更新事件");
+            }
+
+            @Override
+            public void onBatchUpdate(Collection<?> models) {
+                OracleBaseTest.log.info("更新事件");
+            }
+
+            @Override
+            public void onReplace(Object entity) {
+                OracleBaseTest.log.info("替换事件");
+            }
+
+            @Override
+            public void onBatchReplace(Collection<?> models) {
+                OracleBaseTest.log.info("替换事件");
+            }
+
+            @Override
+            public void onEzUpdate(EzUpdate ezUpdate) {
+                OracleBaseTest.log.info("ezUpdate事件");
+            }
+
+            @Override
+            public void onEzBatchUpdate(Collection<EzUpdate> ezUpdates) {
+                ezUpdates.forEach(this::onEzUpdate);
+            }
+        });
+        EzMybatisContent.addOnBuildSqlGetFieldListener(ezMybatisConfig, (originObj, ntType, field, value) -> {
+            log.info("构建sql时获取{}类的{}属性值为{}", ntType.getSimpleName(), field.getName(), value);
+            return value;
+        });
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+    }
+}
